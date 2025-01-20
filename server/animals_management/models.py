48bf3e200f8cd2bf1df0fb94e8558bb3537f14d6
Ruslan Hedz <ruslangedz76@gmail.com>
2025-01-20 18:38:31 +0100

    Animals are added part2

---
+++ server/animals_management/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from reg_log.models import UserProfile
+import os
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+
+
+def rename_photo(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{instance.id}.{ext}" if instance.id else f"temp.{ext}"
+    return os.path.join('animals/', filename)
+
+class Animal(models.Model):
+    SEX_CHOICES = [
+        ('M', 'male'),
+        ('F', 'female')
+    ]
+    STATUS_CHOICES = [
+        ('adopted', 'Adopted'),
+        ('available', 'Available'),
+    ]
+
+    photo = models.ImageField(upload_to=rename_photo)
+    name = models.CharField(max_length=50)
+    species = models.CharField(max_length=25)
+    breed = models.CharField(max_length=25)
+    age = models.IntegerField(default=0)
+    sex = models.CharField(max_length=7, choices=SEX_CHOICES)
+    description = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='available')
+    shelterId = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role': 'shelter'},
+        related_name='animals'
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.species})"
+
+
+@receiver(post_save, sender=Animal)
+def rename_photo_on_save(sender, instance, created, **kwargs):
+    if created and instance.photo:
+        ext = instance.photo.name.split('.')[-1]
+        new_filename = f"{instance.id}.{ext}"
+        new_path = os.path.join('animals/', new_filename)
+        old_path = instance.photo.path
+        new_full_path = os.path.join(settings.MEDIA_ROOT, new_path)
+
+        if old_path != new_full_path:
+            os.rename(old_path, new_full_path)
+            instance.photo.name = new_path
+            instance.save(update_fields=['photo'])
+
+class Adoptions(models.Model):
+    AnimalId = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    AdopterId = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role': 'adopter'},
+        related_name='adoptions'
+    )
+    AdoptionDate = models.DateField
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('pending', 'Pending'),
+            ('approved', 'Approved'),
+            ('rejected', 'Rejected')
+        ],
+        default='pending'
+    )
---
+++ server/animals_management/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import AddAnimalAPIView, CreateAdoptionRequestView, AvailableAnimalsView, MyAnimalsView, AdoptionRequestsView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    #path('api/upload-photo/', UploadPhotoAPIView.as_view(), name='upload-photo'),
+    path('api/add-animal/', AddAnimalAPIView.as_view(), name='add-animal'),
+    path('api/adoption-request/', CreateAdoptionRequestView.as_view(), name='create-adoption-request'),
+    path('api/animals/available/', AvailableAnimalsView.as_view(), name='available-animals'),
+    path('api/animals/my', MyAnimalsView.as_view(), name='my-animals'),
+    path('api/adoption-requests/', AdoptionRequestsView.as_view(), name='create-adoption-requests'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
